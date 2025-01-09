92af9f0f450e7870b7bdec491fcb81d9c6c63a2d
yurkevichkirill <kirill.iurkievich@mail.ru>
2025-01-09 20:42:18 +0300

    Learned about how to skip tests and make a simple repeater function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0) return "ERROR";
+    if(!string) return "";
+    let newStr = "";
+    for(let i = 0; i < num; i++){
+        newStr += string;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
