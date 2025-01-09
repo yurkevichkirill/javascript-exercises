f3802702e5df26b0ad37e84f1ce3e4d07957f5aa
yurkevichkirill <kirill.iurkievich@mail.ru>
2025-01-09 23:32:29 +0300

    Create a function that count sum betwin two values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(begin, end) {
+    if(typeof begin != 'number' || typeof end != 'number' || begin < 0 || end < 0 ||
+        !Number.isInteger(begin) || !Number.isInteger(begin)){
+        return "ERROR";
+    }
+    return (begin < end)?(begin + end) * (end - begin + 1) / 2 : (begin + end) * (begin - end + 1) / 2;
 };
 
 // Do not edit below this line
