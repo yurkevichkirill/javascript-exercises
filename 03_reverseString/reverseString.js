3215fdee2c3c881c8a1a830dab160b54ebf0aacb
yurkevichkirill <kirill.iurkievich@mail.ru>
2025-01-09 21:45:19 +0300

    Create a reverse function and repeat materials about tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newStr = str.split("");
+    for(let i = 0; i < str.length / 2; i++){
+        let temp = newStr[i];
+        newStr[i] = newStr[newStr.length - 1 - i];
+        newStr[newStr.length - 1 - i] = temp;
+    }
+    return newStr.join("");
 };
 
 // Do not edit below this line
