69e4c14d2e74f76355d511e757b8073ba4e3b0e3
yurkevichkirill <kirill.iurkievich@mail.ru>
2025-01-10 14:27:11 +0300

    Create a simple calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  array.forEach(value => sum += value);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let res = 1;
+  array.forEach(value => res *= value);
+  return res;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0 || num === 1){
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
