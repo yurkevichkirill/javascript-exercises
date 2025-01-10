a733f036ef531d9a7db37934d744e987c29983fd
yurkevichkirill <kirill.iurkievich@mail.ru>
2025-01-10 14:58:02 +0300

    Repeat array's and string's methods and functions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // arr = str.split("");
+    // for(let i = 0; i < arr.length; i++){
+    //     if(arr[i] === ' ' || arr[i] === ',' || arr[i] === '.' || arr[i] === '!' || arr[i] === '?'){
+    //         arr.splice(i--, 1);
+    //     }
+    //     arr[i] = arr[i].toLowerCase();
+    // }
 
+    // for(let i = 0; i < arr.length / 2; i++){
+    //     if(arr[i] != arr[arr.length - 1 - i]){
+    //         return false;
+    //     }
+    // }
+    // return true;
+    const alphanumerical = "qwertyuiopasdfghjklzxcvbnm0123456789";
+
+    const cleanedStr = str.toLowerCase().split("").filter(sign => alphanumerical.includes(sign)).join("");
+    const reverseStr = cleanedStr.split("").reverse().join("");
+    return cleanedStr === reverseStr;
 };
 
 // Do not edit below this line
