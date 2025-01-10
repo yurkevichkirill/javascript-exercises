3921a5a943bed8736c919de2dcfc38e4bc2bb2d7
yurkevichkirill <kirill.iurkievich@mail.ru>
2025-01-10 15:23:50 +0300

    Remember fibonacci problem

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0) return "OOPS";
+    if(num == 0) return 0;
+    let fib1 = 1;
+    let fib2 = 1;
+    for(let i = 2; i < num; i++){
+        let temp = fib1;
+        fib1 = fib2;
+        fib2 += temp;
+    }
+    return fib2;
 };
 
 // Do not edit below this line
