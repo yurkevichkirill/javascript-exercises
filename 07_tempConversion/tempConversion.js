629473bdbe977a780697fef3080637f52ba701f8
yurkevichkirill <kirill.iurkievich@mail.ru>
2025-01-10 13:58:15 +0300

    Create a function to convert fahrenheit to celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  return Math.round(10 * (fahr - 32) * 5/9) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  return Math.round(10 * (cels * 9/5 + 32)) / 10;
 };
 
 // Do not edit below this line
