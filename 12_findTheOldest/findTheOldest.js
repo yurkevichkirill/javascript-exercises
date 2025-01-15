4e3c38f9f0a3860a122454107d88d01682e386eb
yurkevichkirill <kirill.iurkievich@mail.ru>
2025-01-15 13:12:32 +0300

    Solve a problem with the use of sort() method

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+   people.sort((a, b) => {
+    const aAge = (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+    const bAge = (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
+    return bAge - aAge;
+   });
+   return people[0];
 };
 
 // Do not edit below this line
